a9fdbc0db039a387c28d211db57101716799de32
liyuanwu <16619891743@163.com>
2023-04-26 00:08:43 +0800

    phpunit

--- tests/TestMiddleware.php
+++ tests/TestMiddleware.php
@@ -2,60 +2,54 @@
 
 namespace mszl\core;
 
+use Exception;
 use mszl\core\middleware\AbstractMiddleware;
 use mszl\core\middleware\MiddlewareStack;
+use PHPUnit\Framework\TestCase;
 
-class TestMiddleware extends \PHPUnit\Framework\TestCase
+class TestMiddleware extends TestCase
 {
-    public function testSomething()
+    public function testEngine()
     {
+        $middlewares = [
+            new TimeMiddleware(),
+            new ErrorHandlerMiddleware()
+        ];
+        var_dump(111111);
+        Engine::getInstance("")->addMiddleware(...$middlewares)->run();
         $this->assertTrue(true);
     }
 }
-//
-//class TimeMiddleware extends AbstractMiddleware
-//{
-//    public function handle($request, MiddlewareStack $stack)
-//    {
-//        $start = microtime(true);
-//
-//        $response = $stack->next($request);
-//
-//        $end = microtime(true);
-//        $time = $end - $start;
-//
-//        echo sprintf('Request time: %s sec' . PHP_EOL, $time);
-//
-//        return $response;
-//    }
-//
-//}
-//
-//class ErrorHandlerMiddleware extends AbstractMiddleware
-//{
-//    public function handle($request, MiddlewareStack $stack)
-//    {
-//        echo "我处理了业务程序", PHP_EOL;
-//        usleep(100 * 215);
-//        return $stack->next($request);
-//    }
-//}
-//
-//class OrderMiddleware extends AbstractMiddleware
-//{
-//
-//    public function handle($request, MiddlewareStack $stack)
-//    {
-//        // TODO: Implement handle() method.
-//        echo "我处理了订单业务", PHP_EOL;
-//        throw new \Exception("我执行超时了");
-//        return $stack->next($request);
-//    }
-//}
-//
-//$middlewares = [
-//    new TimeMiddleware(),
-//    new ErrorHandlerMiddleware()
-//];
-//
-//\mszlu\core\Engine::getInstance("")->addMiddleware(...$middlewares)->run();
+
+class TimeMiddleware extends AbstractMiddleware
+{
+    public function handle($request, MiddlewareStack $stack)
+    {
+        $start = microtime(true);
+
+        $response = $stack->next($request);
+
+        $end = microtime(true);
+        $time = $end - $start;
+
+        echo sprintf('Request time: %s sec' . PHP_EOL, $time);
+
+        return $response;
+    }
+
+}
+
+class ErrorHandlerMiddleware extends AbstractMiddleware
+{
+    /**
+     * @throws Exception
+     */
+    public function handle($request, MiddlewareStack $stack)
+    {
+        echo "我处理了业务程序", PHP_EOL;
+        usleep(100 * 215);
+        if (rand(0, 1)) throw new Exception("我执行超时了");
+        return $stack->next($request);
+    }
+}
+
