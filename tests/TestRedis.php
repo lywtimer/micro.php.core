13602603a36566be9db66682b59ff9b1a89abf84
liyuanwu <16619891743@163.com>
2023-05-07 23:57:10 +0800

    no message

--- tests/TestRedis.php
+++ tests/TestRedis.php
@@ -6,16 +6,39 @@ use PHPUnit\Framework\TestCase;
 
 class TestRedis extends TestCase
 {
-    public function testQuery()
+    public function testConnect()
     {
-        $client = new \Predis\Client([
-            'scheme' => 'tcp',
-            'host' => '8.142.149.58',
-            'port' => 6379,
-        ]);
-        $rs = $client->ping();
-        console($rs);
-        $client->set('foo', 'bar');
-        $value = $client->get('foo');
+        $connect = true;
+        try {
+            $client = new \Predis\Client([
+                'scheme' => 'tcp',
+                'host' => '',
+                'port' => 6379,
+            ]);
+            $client->auth("");
+            $rs = $client->ping();
+            console($rs . "");
+
+            $client->set('foo', 'bar');
+            $value = $client->get('foo');
+            console($value);
+        } catch (\Exception $e) {
+            $connect = false;
+            console("predis connect error : " . $e->getMessage());
+        }
+
+        try {
+            $redis = new \Redis();
+            $redis->connect("");
+            $redis->auth("");
+            $rs = $redis->ping();
+            console($rs);
+            $redis->hSet("cart:1001", "name", "xiaoming");
+        } catch (\Exception $e) {
+            $connect = false;
+            console("redis connect error : " . $e->getMessage());
+        }
+
+        $this->assertIsBool($connect);
     }
 }
